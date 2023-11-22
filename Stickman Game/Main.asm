dfb0b2911865d04d39ef59a971d2784239c8bcd2
Hammad Zafar Bawara <51308210+hammadbawara@users.noreply.github.com>
2023-11-22 18:08:39 +0500

    Add project files.

---
+++ Stickman Game/Main.asm
@@ -0,0 +1,135 @@
+INCLUDELIB C:\Irvine\Irvine32.lib
+INCLUDE C:\Irvine\Irvine32.inc
+
+.data
+	rows           EQU 15
+	columns        EQU 44
+	screen2D       BYTE rows*columns dup(32), 0
+
+	newLine        BYTE 0dh
+	carriageReturn BYTE 0ah
+
+	screenBoundary  BYTE "========================================",0dh
+
+
+.code
+
+; Put String into the screen
+PrintAt PROC
+	; x position of screen
+	; y position of screen
+	; size of string
+	; offset of string
+
+	LOCAL x: DWORD, y: DWORD
+	; moving parameters into x and y
+	MOV eax, [ebp + 20]
+	MOV x, eax
+	MOV eax, [ebp + 16]
+	MOV y, eax
+
+	; moving length of string into ecx
+	MOV ecx, [ebp + 12]
+
+	; moving offset of array
+	MOV edx, [ebp + 8]
+	; iterator for array
+	MOV ebx, 0
+
+	LoopStart:
+		PUSH ecx
+		PUSH edx
+		; calculating point of screen
+		MOV eax, y
+		MOV ecx, columns
+		MUL ecx
+		ADD eax, x
+
+		POP edx
+
+		; if it is 0dh then move to next line
+		mov cl, 0dh
+		CMP [edx + ebx], cl
+		JE newLineCame
+		
+		; moving character into screen2D
+		MOV cl, [edx + ebx]
+		MOV screen2D[eax], cl
+		
+		INC x
+		INC ebx
+
+		POP ecx
+		LOOP LoopStart
+		JMP endprocedure
+	newLineCame:
+			INC y
+			MOV eax, [ebp + 20]
+			MOV x, eax
+			INC ebx
+
+			POP ecx
+			LOOP LoopStart
+
+	endprocedure:
+	ret 16
+
+PrintAt ENDP
+
+InitializeScreen PROC
+	MOV eax, 0
+	MOV ecx, rows
+	
+	LoopStart:
+		; putting '|' on start and end of screen boundary
+		MOV screen2D[eax], '|'
+		ADD eax, columns-3
+		MOV screen2D[eax], '|'
+		INC eax
+
+		; putting new line character at the end of line
+		MOV screen2D[eax], 0dh      
+		INC eax
+		MOV screen2D[eax], 0ah
+		INC eax
+		LOOP LoopStart
+
+	; displaying upper boundary
+	push 1
+	push 0
+	push sizeof screenBoundary
+	push offset screenBoundary
+	Call PrintAt
+	
+	; displaying lower boundary
+	push 1
+	push rows-1
+	push sizeof screenBoundary
+	push offset screenBoundary
+	CALL PrintAt
+
+	ret
+
+InitializeScreen ENDP
+
+DislayScreen PROC
+
+	MOV edx, OFFSET screen2D
+	CALL WriteString
+	ret
+
+DislayScreen ENDP
+	
+
+main PROC
+
+	CALL InitializeScreen
+	
+	
+	
+	CALL DislayScreen
+
+	INVOKE ExitProcess, 0
+
+main endp
+end main
